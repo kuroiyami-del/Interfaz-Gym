9a19b7e05eca119fc775ee4ea6304fe5b372f0cd
Kuro <kuroiyami720@gmail.com>
2025-01-29 16:51:54 -0500

    Se agregaron funciones para usar en main y se quitó el else del final del flujo principal

--- database/db_operaciones.py
+++ database/db_operaciones.py
@@ -47,6 +47,91 @@ def eliminarUsuario(cedula):
     finally:
         conn.close()
 
+def usuarioEspecifico(cedula):
+    conn = connect()
+    cursor = conn.cursor()
+
+    try:
+
+        cursor.execute("SELECT * FROM usuarios WHERE cedula = ? ",(cedula,))
+        resultado = cursor.fetchone()
+        
+
+        if resultado is None:
+                print(f"Usuario con cédula {cedula} no existe.")
+                return None
+
+          # Obtener nombres de las columnas
+        columnas = [desc[0] for desc in cursor.description]
+        
+        # Crear un diccionario con los nombres de las columnas y los valores
+        usuario_dict = dict(zip(columnas, resultado))
+        
+        return usuario_dict  
+
+    except sqlite3.Error as e:
+        print(f"Error al elcontrar el usuario: {e}")
+    finally:
+        conn.close()
+
+
+def modificarUsuario(cedula):
+    conn = connect()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM usuarios WHERE cedula = ? ", (cedula,))
+        usuario = cursor.fetchone()
+
+        if usuario is None:
+            print(f'⚠️ Usuario con cédula {cedula} no existe.')
+            return 
+
+          # Obtener nombres de las columnas
+        columnas = [desc[0] for desc in cursor.description]
+
+        #Mostrar datos actuales del usuario
+
+        print(f"\nInformacion actual del usuario:")
+        for clave,valor in zip(columnas,usuario):
+            print(f"{clave.capitalize()}: {valor}")
+
+        # Lista de opciones disponibles para modificar
+        opciones = {
+            "1": "nombre",
+            "2": "edad",
+            "3": "telefono",
+            "4": "foto",
+            "5": "direccion",
+            "6": "plan"
+        }
+
+        print("\nQue dato deseas cambiar?: ")
+        for key, value in opciones.items():
+            print(f'{key}. {value}')
+
+        opcion = input("Ingrese la opcion: ")
+
+        if opcion not in opciones:
+            print("⚠️ Opción inválida.")
+            return
+        
+        nuevo_valor = input(f"Ingrese el nuevo valor para {opciones[opcion]}: ")
+
+        cursor.execute(f'UPDATE usuarios SET {opciones[opcion]} = ? WHERE cedula = ?', (nuevo_valor,cedula))
+        conn.commit()
+
+        print("\n✅ Datos actualizados con éxito.")
+
+    except sqlite3.Error as e:
+        print(f"❌ Error al modificar el usuario: {e}")
+        
+    finally:
+        conn.close()
+
+        
+
+
 
 
 
--- main.py
+++ main.py
@@ -6,8 +6,10 @@ def main():
     crearDataBase()
     while True:
         print("1. Crear un nuevo usuario ")
-        print("2. Ver usuarios ")
+        print("2. Ver todos los usuarios ")
         print("3. Eliminar usuario ")
+        print("4. Buscar un usuario especifico ")
+        print("5. Alterar usuario ")
         print("0. Salir")
         
         opcion = int(input("Inserta tu opcion: "))
@@ -21,7 +23,7 @@ def main():
                 direccion = input("Direccion: ")
                 plan = input("Ingrese el tipo de plan: ")
 
-                insertarUsuario(nombre, edad, telefono, foto, cedula,direccion,plan)
+                insertarUsuario(nombre, edad, telefono, foto, cedula, direccion, plan)
                 print("\nUsuario agregado con éxito.")
 
         if opcion == 2:
@@ -32,13 +34,24 @@ def main():
         if opcion == 3:
                 cedula = input("Ingrese la cédula del usuario a eliminar: ")
                 eliminarUsuario(cedula)
-                
+        
+        if opcion == 4:
+                cedula = input("Ingrese la cédula del usuario que desea buscar: ")
+                usuarios = usuarioEspecifico(cedula)
+
+                if usuarios:   
+                    print("\nInformación del usuario:")
+                for clave, valor in usuarios.items():
+                        print(f'{clave.capitalize()}: {valor}')
+
+        if opcion == 5:
+            cedula = input("Ingrese la cédula del usuario a modificar: ")
+            modificarUsuario(cedula)
 
         if opcion == 0:
             print("Saliendo del programa")
             break
-        else:
-            print("\nOpcion invalida")
+        
 
 if __name__ == "__main__":
     main()
